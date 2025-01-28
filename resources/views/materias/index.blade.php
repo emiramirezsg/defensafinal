d52f537258e0fada07edb0962f60b2059915f10e
emiramirezsg <emilyramirezsoriagalvarro@gmail.com>
2025-01-28 09:10:29 -0400

    errores

--- resources/views/materias/index.blade.php
+++ resources/views/materias/index.blade.php
@@ -174,7 +174,7 @@
                                             <input type="text" id="nombre-{{ $materia->id }}" name="nombre" value="{{ $materia->nombre }}" class="form-control" required>
                                         </div>
                                         <div class="form-group">
-                                            <label for="horas_semana">Horas Mensuales:</label>
+                                            <label for="horas_semana">Horas a la semana:</label>
                                             <input type="number" name="horas_semana" id="horas_semana" value="{{ $materia->horas_semana }}" required>
                                         </div>
                                         <div class="form-group">
